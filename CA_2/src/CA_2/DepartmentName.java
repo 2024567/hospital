fa3daabd56c657e2365b426703b0ca4d33221ea7
sorgilgerel <98004417+sorgilgerel@users.noreply.github.com>
2025-05-08 15:34:19 +0100

    added managertype and DepartmentName enums for Hostpital structure

---
+++ CA_2/src/CA_2/DepartmentName.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author sorgi
+ */
+public enum DepartmentName {
+    EMERGENCY,
+    PEDIATRICS,
+    SURGERY
+}
---
+++ CA_2/src/CA_2/ManagerType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author sorgi
+ */
+public enum ManagerType {
+    CHIEF_SURGEON,
+    DEPARTMENT_HEAD,
+    NURSE_SUPERVISOR
+}
